deb9bb2e18fb5b45408bb9f312ecd3051b985795
saka <laurinfrank03@gmail.com>
2024-11-12 14:37:11 +0100

    changed Docker files and added env var for auth token expiration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
-
-# Authenticate GitHub requests with PAT
-echo "Authenticating with GitHub..."
+echo "INFO | Importing Environment Variables from .env"
 if [ -f /app/.env ]; then
     export $(grep -v '^#' /app/.env | xargs)
 fi
-
+REPO_DIR="/app/repository"
 REPO_URL="https://${GITHUB_PAT}@github.com/${GITHUB_USERNAME}/${GITHUB_REPOSITORY}.git"
-echo "URL: $REPO_URL"
 
 # Clone the repository if it's not already present
-if [ ! -d ".git" ]; then
-  echo "Initializing the repository..."
-  git remote add origin "$REPO_URL"
-  echo "Cloning the repository..."
-  git clone --single-branch --branch production app
+if [ ! -d "repository/.git" ]; then
+  git clone --single-branch --branch production $REPO_URL repository
+  echo "INFO | Installing Project Dependencies..."
+  cd $REPO_DIR && bun install
 else
+  cd $REPO_DIR
+  echo "INFO | Repository already exists, setting the remote origin"
   git remote set-url origin "$REPO_URL"
-  echo "Repository already exists. Fetching the latest changes..."
-  echo "Checking for updates in the production branch..."
+  echo "INFO | Checking for updates in the production branch..."
   git fetch origin production
   # Check if local production branch is up-to-date
   LOCAL_HASH=$(git rev-parse HEAD)
   REMOTE_HASH=$(git rev-parse origin/production)
+  echo "INFO | LOCAL : ${LOCAL_HASH}"
+  echo "INFO | REMOTE: ${REMOTE_HASH}"
 
   if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
-    echo "Changes detected in the production branch. Pulling latest changes..."
+    echo "INFO | Changes detected in the production branch. Pulling latest changes..."
     git pull origin production
+    echo "INFO | Installing Project Dependencies..."
     bun install
-    bun run build
   else
-    echo "No changes detected. Starting the server..."
+    echo "INFO | No changes detected. Starting the server..."
+    bun run .output/server/index.mjs
+    exit 0
   fi
 fi
 
-if [ ! -f ".output/server/index.mjs" ]; then
-  echo "Building the project..."
+cd $REPO_DIR
+if [ ! -f ".output/server/index.mjs" ] ; then
+  echo "INFO | Building the project..."
   bun run build
 fi
 
-# Start the Nuxt server
+echo "INFO | Starting the server..."
 bun run .output/server/index.mjs
