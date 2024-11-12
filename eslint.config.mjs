3f2fc88a8d2f555a22a86d083e1420cf570fb583
saka <laurinfrank03@gmail.com>
2024-11-12 05:42:03 +0100

    changed akit

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
-
-export default withNuxt({
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-        'no-console': 'off' // allow console.log in TypeScript files
-    }
-})
--- server/api/files/[fileId]/download.ts
+++
@@ -1,8 +0,0 @@
-import { getQuery } from 'h3'; // Import getQuery from h3
-
-export default defineEventHandler(async (event) => {
-    const query = getQuery(event); // Use getQuery to extract query parameters
-    const token = query.token; // Extract the token from the query
-
-    return "";
-});
