e1ac8a43d223d5fc35bd0e4b174d700c0b670bac
sak0a <laurinfrank03@gmail.com>
2024-11-14 18:13:16 +0100

    removed temporarily the content for handling files

--- server/api/files/[fileId].ts
+++ server/api/files/[fileId].ts
@@ -1,9 +1,5 @@
 
 
 export default defineEventHandler(async (event) => {
-    const fileId = event.context.params.fileId;
-    const { password } = await readBody(event); // Password entered by user
-    // Retrieve metadata to check if a password is required
-    // If a password is required, validate it and issue a JWT token
 
 });
