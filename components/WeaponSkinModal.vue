bb1f6b55d3531b08e1c2b2090459fbd88a2f0a01
sak0a <laurinfrank03@gmail.com>
2025-12-15 23:47:01 +0100

    feat: add validation for weapon skin configuration before generating inspect link in WeaponSkinModal component

--- components/WeaponSkinModal.vue
+++ components/WeaponSkinModal.vue
@@ -360,6 +360,13 @@ const handleCreateInspectLink = async () => {
     return
   }
 
+  // Require a configured skin (paint index) before generating an inspect link
+  if (!customization.value.paintIndex || customization.value.paintIndex === 0) {
+    state.value.error = 'No skin configured for inspect link creation'
+    emit('error', state.value.error)
+    return
+  }
+
   try {
     state.value.isLoadingInspect = true
     state.value.error = null
@@ -770,7 +777,13 @@ watch(() => props.weapon, () => {
       <NDivider vertical />
 
       <!-- Generate Weapon Inspect Link by Data -->
-      <NButton :loading="state.isLoadingInspect" secondary type="default" :disabled="!selectedSkin" @click="handleCreateInspectLink">
+      <NButton
+        :loading="state.isLoadingInspect"
+        secondary
+        type="default"
+        :disabled="!selectedSkin || customization.paintIndex === 0"
+        @click="handleCreateInspectLink"
+      >
         <template #icon>
           <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-zoom-scan"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 8v-2a2 2 0 0 1 2 -2h2" /><path d="M4 16v2a2 2 0 0 0 2 2h2" /><path d="M16 4h2a2 2 0 0 1 2 2v2" /><path d="M16 20h2a2 2 0 0 0 2 -2v-2" /><path d="M8 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M16 16l-2.5 -2.5" /></svg>
         </template>
