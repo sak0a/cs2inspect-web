94722151a462efa1f1460ea441cea1e1ffe114dc
sak0a <laurinfrank03@gmail.com>
2026-01-19 03:45:10 +0100

    chore: update Nuxt configuration and dependencies, remove unused Tailwind CSS files, and integrate shadcn for improved UI component management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -113,12 +113,12 @@ export default defineNuxtConfig({
     ]
   },
   tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+    /*cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
     exposeConfig: {
       level: 2
     },
     config: {},
-    viewer: false,
+    viewer: false,*/
   },
   modules: [
     '@nuxtjs/tailwindcss',
@@ -128,14 +128,19 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@pinia/nuxt',
     'nuxt-i18n-micro',
-    '@nuxtjs/seo',
+    //'@nuxtjs/seo',
     '@vite-pwa/nuxt',
+    'shadcn-nuxt'
   ],
   site: {
     url: 'https://cs2inspect.com',
     name: 'CS2 Inspect',
     description: 'Inspect CS2 Skins on generic server with any float, pattern and sticker combination.',
     defaultLocale: 'en',
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui',
   },
   runtimeConfig: {
     public: {
